e4d82fd34a125c6bcf462d3a29e56754de4eebd4
FrancoS <ti43243@uvp.edu.mx>
2025-01-17 13:38:42 -0600

    git add . git commit -m "chore: prepare for codemod"

--- src/app/(pages)/app/settings/section_settings/changepassword.tsx
+++ src/app/(pages)/app/settings/section_settings/changepassword.tsx
@@ -3,36 +3,41 @@ import { Input, Button, Label } from "@/components";
 
 export default function PrivacitySettings() {
   return (
-    <div className="space-y-6">
-      <div className="flex items-center gap-4">
+    <div className="space-y-6 max-w-md mx-auto">
+      <div className="space-y-4">
         {/* Cambiar contraseña */}
-        <div className=" w-full">
+        <div>
           <Label htmlFor="password">Contraseña actual</Label>
           <Input
             id="password"
             type="password"
             placeholder="Contraseña actual"
+            className="mt-1 block w-full rounded-xl border-gray-400 dark:border-neutral-700 focus:border-blue-500"
           />
         </div>
-        <div className="w-full">
+        <div>
           <Label htmlFor="newpassword">Nueva contraseña</Label>
           <Input
             id="newpassword"
             type="password"
             placeholder="Nueva contraseña"
+            className="mt-1 block w-full rounded-xl border-gray-400 dark:border-neutral-700 focus:border-primary-focus"
           />
+        </div>
+        <div>
           <Label htmlFor="confirmpassword">Confirmar contraseña</Label>
           <Input
             id="confirmpassword"
             type="password"
             placeholder="Confirmar contraseña"
+            className="mt-1 block w-full rounded-xl border-gray-400 dark:border-neutral-700 focus:border-primary-focus"
           />
         </div>
       </div>
       <Button
         title="Guardar"
         type="submit"
-        className="w-full bg-primaryper hover:bg-primary-hover text-white"
+        className="w-full bg-primaryper hover:bg-primary-hover text-white mt-6"
       >
         Guardar cambios
       </Button>
